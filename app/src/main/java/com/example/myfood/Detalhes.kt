8d72c95f67c5101329d60242ed75e97e83b1e046
gabriellehaner <gabriellehac600@gmail.com>
2023-01-23 09:57:29 -0300

    Edição na tela de detalhes da receita

--- app/src/main/java/com/example/myfood/Detalhes.kt
+++ app/src/main/java/com/example/myfood/Detalhes.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.cadastro_fragment.*
+import kotlinx.android.synthetic.main.detalhes_fragment.*
 
 class Detalhes : Fragment(R.layout.detalhes_fragment) {
 
@@ -16,7 +18,10 @@ class Detalhes : Fragment(R.layout.detalhes_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        clickEdit()
 
+
+        //Receber dados dos campos de cadastro
         val tituloreceita = view.findViewById<TextView>(R.id.recebertituloreceita)
         tituloreceita?.text = args.receita.titulo
 
@@ -30,13 +35,19 @@ class Detalhes : Fragment(R.layout.detalhes_fragment) {
         receberpreparo?.text = args.receita.mododepreparo
 
 
+        recebertituloreceita.isEnabled = false
+        recebertempodepreparo.isEnabled = false
+        receberinngredientes.isEnabled = false
+        recebermododepreparo.isEnabled = false
 
 
+        //Navegacao dos botoes
         view.findViewById<Button>(R.id.botaocvoltarhome_detalhes).setOnClickListener {
             ReceitaDB.deletarReceita(args.receita)
             findNavController().navigate(R.id.detalhes_para_consultar)
         }
 
+        //Acao de atualizar a receita
         view.findViewById<Button>(R.id.botao_atualizar).setOnClickListener {
             ReceitaDB.alterarReceita(args.receita, Receita(
                 titulo = tituloreceita.text.toString(),
@@ -47,6 +58,21 @@ class Detalhes : Fragment(R.layout.detalhes_fragment) {
             findNavController().navigate(R.id.detalhes_para_consultar)
         }
 
+    }
+
+    fun clickEdit() {
+        botaoeditartitulo.setOnClickListener{
+            recebertituloreceita.isEnabled = true
+        }
+        botaoeditartempo.setOnClickListener{
+            recebertempodepreparo.isEnabled = true
+        }
+        botaoeditaringredientes.setOnClickListener{
+            receberinngredientes.isEnabled = true
+        }
+        botaoeditarpreparo.setOnClickListener{
+            recebermododepreparo.isEnabled = true
+        }
     }
 
 }
