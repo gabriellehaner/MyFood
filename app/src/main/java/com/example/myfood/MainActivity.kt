f26aa86bb2dd068f5f666635cd6864af81555886
gabriellehaner <gabriellehac600@gmail.com>
2023-01-23 15:17:05 -0300

    Edição de tela inicial

--- app/src/main/java/com/example/myfood/MainActivity.kt
+++ app/src/main/java/com/example/myfood/MainActivity.kt
@@ -7,6 +7,11 @@ import android.view.View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
+
+        Thread.sleep(2000)
+
+        setTheme(R.style.Theme_MyFood)
+
         supportActionBar!!.hide()
         val decorView = window.decorView
         val uiOptions = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
